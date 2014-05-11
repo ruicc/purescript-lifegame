6ee6507adc43d1703eafe1e5a6877a9d2629b981
Ruichi Kousuke <kou.ruichi@altplus.co.jp>
2014-05-12 02:11:37 +0700

    Divide codes again.

--- public/main.js
+++ public/main.js
@@ -3793,21 +3793,33 @@ PS.Main = (function () {
     "use strict";
     var Prelude = PS.Prelude;
     var Control_Monad_Eff = PS.Control_Monad_Eff;
-    function onLoad(f) {  return function() {    window.onload = f;  };};
-    function setGlobalVars() {     window.SCREEN_SIZE = 500;      window.SIDE_CELLS = 100;      window.CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;      window.FPS = 2;          var field = new Array(SIDE_CELLS*SIDE_CELLS);          var tempField = new Array(SIDE_CELLS*SIDE_CELLS);          for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2);          var canvas = document.getElementById('world');          canvas.width = canvas.height = SCREEN_SIZE;          var scaleRate = Math.min(window.innerHeight/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE);          canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';          var context = canvas.getContext('2d');          context.fillStyle = 'rgb(211, 85, 149)';      window.field = field;    window.tempField = tempField;    window.context = context;};
-    function setIntervalLG() {  setInterval(window.update, 1000/window.FPS, window.field, window.tempField, window.context);};;
-    function setUpdate() {     var draw = function (field, context) {         context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE);          for (var i=0; i<field.length; i++) {             var x = (i%SIDE_CELLS)*CELL_SIZE;              var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE;              if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE);          }      };     var _update = function(field, tempField, context) {        var n = 0;          tempField = field.slice();          for (var i=0; i<tempField.length; i++) {             n = 0;             for (var s=-1; s<2; s++) {                 for (var t=-1; t<2; t++) {                     if (s==0 && t==0) continue;                      var c = i+s*SIDE_CELLS+t;                      if (c>=0 && c<tempField.length) {                          if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) {                              if (tempField[c]) n ++;                          }                     }                 }             }             if (tempField[i] && (n==2||n==3)) {                  field[i] = 1;              } else if (!tempField[i] && n==3) {                  field[i] = 1;              } else field[i] = 0;          }         draw(field, context);      };    window.update = _update;};
-    var main = onLoad(function __do() {
+    function onload(f) {  return function() {    window.onload = f;  };};
+    function setGlobalVars() {     window.SCREEN_SIZE = 500;      window.SIDE_CELLS = 100;      window.CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;  };
+    function startLifegame(fps) {  return function(field) {    return function(update) {      return function(draw) {        return function() {            var next = update(field)();            draw(next)();            setTimeout(startLifegame(fps)(next)(update)(draw), 1000/fps);        };      };    };  };};
+    function update(field) {  return function() {    var n = 0;      var tempField = field.slice();      for (var i=0; i<tempField.length; i++) {       n = 0;       for (var s=-1; s<2; s++) {         for (var t=-1; t<2; t++) {           if (s==0 && t==0) continue;            var c = i+s*SIDE_CELLS+t;            if (c>=0 && c<tempField.length) {              if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) {                if (tempField[c]) n ++;              }           }         }       }       if (tempField[i] && (n==2||n==3)) {          field[i] = 1;        } else if (!tempField[i] && n==3) {          field[i] = 1;        } else field[i] = 0;      }     return field;  }; };
+    function draw(context) {   return function(field) {     return function() {      context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE);       for (var i=0; i<field.length; i++) {         var x = (i%SIDE_CELLS)*CELL_SIZE;         var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE;         if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE);       }     };   }; };
+    function getContext() {  var canvas = document.getElementById('world');    canvas.width = canvas.height = SCREEN_SIZE;    var scaleRate = Math.min(window.innerHeight/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE);    canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';    var context = canvas.getContext('2d');    context.fillStyle = 'rgb(211, 85, 149)';    return context; };
+    function createField(h) {  return function(w) {    return function() {      var field = new Array(h * w);        for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2);        return field;    };  };};
+    var main = onload(function __do() {
         setGlobalVars();
-        setUpdate();
-        return setIntervalLG();
+        var _1 = getContext();
+        return (function (_2) {
+            return function __do() {
+                var _1 = createField(100)(100)();
+                draw(_2)(_1)();
+                return startLifegame(2)(_1)(update)(draw(_2))();
+            };
+        })(_1)();
     });
     return {
         main: main, 
-        setUpdate: setUpdate, 
-        setIntervalLG: setIntervalLG, 
+        createField: createField, 
+        getContext: getContext, 
+        draw: draw, 
+        update: update, 
+        startLifegame: startLifegame, 
         setGlobalVars: setGlobalVars, 
-        onLoad: onLoad
+        onload: onload
     };
 })();
 PS.Main.main();
--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,22 @@
 module Main where
 
---import Data.Either
 import Prelude
 import Data.Foreign
 import Control.Monad.Eff
+import Control.Monad.Eff.Random
 --import qualified Control.Monad.JQuery as J
 --import qualified Graphics.Canvas as C
 --import Debug.Trace
 
 
-foreign import onLoad
-    "function onLoad(f) {\
+-- used by canvas (I don't know it well)
+foreign import data Context :: *
+
+type Field = [Number]
+type FPS = Number
+
+foreign import onload
+    "function onload(f) {\
     \  return function() {\
     \    window.onload = f;\
     \  };\
@@ -21,69 +27,94 @@ foreign import setGlobalVars
     \    window.SCREEN_SIZE = 500;  \
     \    window.SIDE_CELLS = 100;  \
     \    window.CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;  \
-    \    window.FPS = 2;  \
-    \        var field = new Array(SIDE_CELLS*SIDE_CELLS);  \
-    \        var tempField = new Array(SIDE_CELLS*SIDE_CELLS);  \
-    \        for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2);  \
-    \        var canvas = document.getElementById('world');  \
-    \        canvas.width = canvas.height = SCREEN_SIZE;  \
-    \        var scaleRate = Math.min(window.innerHeight/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE);  \
-    \        canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';  \
-    \        var context = canvas.getContext('2d');  \
-    \        context.fillStyle = 'rgb(211, 85, 149)';  \
-    \    window.field = field;\
-    \    window.tempField = tempField;\
-    \    window.context = context;\
     \}" :: forall e a. Eff e {}
 
-foreign import setIntervalLG
-    "function setIntervalLG() {\
-    \  setInterval(window.update, 1000/window.FPS, window.field, window.tempField, window.context);\
-    \};" :: forall e. Eff e {}
+foreign import startLifegame
+    "function startLifegame(fps) {\
+    \  return function(field) {\
+    \    return function(update) {\
+    \      return function(draw) {\
+    \        return function() {\
+    \            var next = update(field)();\
+    \            draw(next)();\
+    \            setTimeout(startLifegame(fps)(next)(update)(draw), 1000/fps);\
+    \        };\
+    \      };\
+    \    };\
+    \  };\
+    \}" :: forall e. FPS -> Field -> (Field -> Field) -> (Field -> Eff e {}) -> Eff e {}
 
---    \};" :: forall a b c e. (a -> b -> c -> Eff e {}) -> Eff e {}
-
-foreign import setUpdate
-    "function setUpdate() { \
-    \    var draw = function (field, context) { \
-    \        context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE);  \
-    \        for (var i=0; i<field.length; i++) { \
-    \            var x = (i%SIDE_CELLS)*CELL_SIZE;  \
-    \            var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE;  \
-    \            if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE);  \
-    \        }  \
-    \    }; \
-    \    var _update = function(field, tempField, context) {\
-    \        var n = 0;  \
-    \        tempField = field.slice();  \
-    \        for (var i=0; i<tempField.length; i++) { \
-    \            n = 0; \
-    \            for (var s=-1; s<2; s++) { \
-    \                for (var t=-1; t<2; t++) { \
-    \                    if (s==0 && t==0) continue;  \
-    \                    var c = i+s*SIDE_CELLS+t;  \
-    \                    if (c>=0 && c<tempField.length) {  \
-    \                        if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) {  \
-    \                            if (tempField[c]) n ++;  \
-    \                        } \
-    \                    } \
-    \                } \
+foreign import update
+    "function update(field) {\
+    \  return function() {\
+    \    var n = 0;  \
+    \    var tempField = field.slice();  \
+    \    for (var i=0; i<tempField.length; i++) { \
+    \      n = 0; \
+    \      for (var s=-1; s<2; s++) { \
+    \        for (var t=-1; t<2; t++) { \
+    \          if (s==0 && t==0) continue;  \
+    \          var c = i+s*SIDE_CELLS+t;  \
+    \          if (c>=0 && c<tempField.length) {  \
+    \            if (i<c && c%SIDE_CELLS!=0 || i>c && c%SIDE_CELLS!=SIDE_CELLS-1) {  \
+    \              if (tempField[c]) n ++;  \
     \            } \
-    \            if (tempField[i] && (n==2||n==3)) {  \
-    \                field[i] = 1;  \
-    \            } else if (!tempField[i] && n==3) {  \
-    \                field[i] = 1;  \
-    \            } else field[i] = 0;  \
+    \          } \
     \        } \
-    \        draw(field, context);  \
+    \      } \
+    \      if (tempField[i] && (n==2||n==3)) {  \
+    \        field[i] = 1;  \
+    \      } else if (!tempField[i] && n==3) {  \
+    \        field[i] = 1;  \
+    \      } else field[i] = 0;  \
+    \    } \
+    \    return field;\
+    \  }; \
+    \}" :: Field -> Field
+
+foreign import draw
+    "function draw(context) { \
+    \  return function(field) { \
+    \    return function() {\
+    \      context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE); \
+    \      for (var i=0; i<field.length; i++) { \
+    \        var x = (i%SIDE_CELLS)*CELL_SIZE; \
+    \        var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE; \
+    \        if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE); \
+    \      } \
     \    }; \
-    \ \
-    \  window.update = _update;\
-    \}" :: forall e. Eff e {}
+    \  }; \
+    \}" :: forall e. Context -> Field -> Eff e {}
 
+-- Dependent html.
+foreign import getContext
+    "function getContext() {\
+    \  var canvas = document.getElementById('world');  \
+    \  canvas.width = canvas.height = SCREEN_SIZE;  \
+    \  var scaleRate = Math.min(window.innerHeight/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE);  \
+    \  canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';  \
+    \  var context = canvas.getContext('2d');  \
+    \  context.fillStyle = 'rgb(211, 85, 149)';  \
+    \  return context; \
+    \}" :: forall e. Eff e Context
     
+foreign import createField
+    "function createField(h) {\
+    \  return function(w) {\
+    \    return function() {\
+    \      var field = new Array(h * w);  \
+    \      for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2);  \
+    \      return field;\
+    \    };\
+    \  };\
+    \}" :: forall r. Number -> Number -> Eff (random :: Random | r) Field
 
-main = onLoad $ do
+
+main = onload $ do
+    let fps = 2
+    let cellNum = 100
     setGlobalVars
-    setUpdate
-    setIntervalLG
+    ctx <- getContext
+    initField <- createField cellNum cellNum
+    draw ctx initField
+    startLifegame fps initField update (draw ctx)
