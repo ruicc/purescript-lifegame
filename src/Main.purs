2c3ed87fe21864bd4d9a2ac621e93d180780738b
Ruichi Kousuke <kou.ruichi@altplus.co.jp>
2014-05-12 00:42:18 +0700

    Divide codes into some functions.

--- src/Main.purs
+++ src/Main.purs
@@ -8,31 +8,52 @@ import Control.Monad.Eff
 --import qualified Graphics.Canvas as C
 --import Debug.Trace
 
-main = onload
 
-foreign import onload
-    "(function () { \
-    \    var SCREEN_SIZE = 500;  \
-    \    var SIDE_CELLS = 100;  \
-    \    var CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;  \
-    \    var FPS = 10;  \
-    \    var canvas;  \
-    \    var context;  \
-    \ \
-    \    window.onload = function() { \
+foreign import onLoad
+    "function onLoad(f) {\
+    \  return function() {\
+    \    window.onload = f;\
+    \  };\
+    \}" :: forall e a. Eff e a -> Eff e {}
+
+foreign import setGlobalVars
+    "function setGlobalVars() { \
+    \    window.SCREEN_SIZE = 500;  \
+    \    window.SIDE_CELLS = 100;  \
+    \    window.CELL_SIZE = SCREEN_SIZE / SIDE_CELLS;  \
+    \    window.FPS = 2;  \
     \        var field = new Array(SIDE_CELLS*SIDE_CELLS);  \
     \        var tempField = new Array(SIDE_CELLS*SIDE_CELLS);  \
     \        for (var i=0; i<field.length; i++) field[i] = Math.floor(Math.random()*2);  \
-    \        canvas = document.getElementById('world');  \
+    \        var canvas = document.getElementById('world');  \
     \        canvas.width = canvas.height = SCREEN_SIZE;  \
     \        var scaleRate = Math.min(window.innerHeight/SCREEN_SIZE, window.innerHeight/SCREEN_SIZE);  \
     \        canvas.style.width = canvas.style.height = SCREEN_SIZE*scaleRate+'px';  \
-    \        context = canvas.getContext('2d');  \
+    \        var context = canvas.getContext('2d');  \
     \        context.fillStyle = 'rgb(211, 85, 149)';  \
-    \        update(field, tempField);  \
+    \    window.field = field;\
+    \    window.tempField = tempField;\
+    \    window.context = context;\
+    \}" :: forall e a. Eff e {}
+
+foreign import setIntervalLG
+    "function setIntervalLG() {\
+    \  setInterval(window.update, 1000/window.FPS, window.field, window.tempField, window.context);\
+    \};" :: forall e. Eff e {}
+
+--    \};" :: forall a b c e. (a -> b -> c -> Eff e {}) -> Eff e {}
+
+foreign import setUpdate
+    "function setUpdate() { \
+    \    var draw = function (field, context) { \
+    \        context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE);  \
+    \        for (var i=0; i<field.length; i++) { \
+    \            var x = (i%SIDE_CELLS)*CELL_SIZE;  \
+    \            var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE;  \
+    \            if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE);  \
+    \        }  \
     \    }; \
-    \ \
-    \    function update(field, tempField) { \
+    \    var _update = function(field, tempField, context) {\
     \        var n = 0;  \
     \        tempField = field.slice();  \
     \        for (var i=0; i<tempField.length; i++) { \
@@ -54,16 +75,15 @@ foreign import onload
     \                field[i] = 1;  \
     \            } else field[i] = 0;  \
     \        } \
-    \        draw(field);  \
-    \        setTimeout(update, 1000/FPS, field, tempField);  \
+    \        draw(field, context);  \
     \    }; \
     \ \
-    \    function draw(field) { \
-    \        context.clearRect(0, 0, SCREEN_SIZE, SCREEN_SIZE);  \
-    \        for (var i=0; i<field.length; i++) { \
-    \            var x = (i%SIDE_CELLS)*CELL_SIZE;  \
-    \            var y = Math.floor((i/SIDE_CELLS))*CELL_SIZE;  \
-    \            if (field[i]) context.fillRect(x, y, CELL_SIZE, CELL_SIZE);  \
-    \        }  \
-    \    }; \
-    \})();" :: forall e a. Eff e a
+    \  window.update = _update;\
+    \}" :: forall e. Eff e {}
+
+    
+
+main = onLoad $ do
+    setGlobalVars
+    setUpdate
+    setIntervalLG
